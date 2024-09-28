4f27a97d615b7078f9f0c790d428877cac9b3b21
Tristan F. <tristanf@reed.edu>
2024-09-27 21:04:58 -0700

    feat: bat (#9)
    catbat bat cat

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [ git vim fastfetch dust ];
+  environment.systemPackages = with pkgs; [ git vim fastfetch dust bat ];
 
   services.tailscale = {
     enable = true;
