c6a5cde67eff2f7356de27a5e9644968ca9701ab
Tristan F. <tristanf@reed.edu>
2024-09-27 21:38:41 -0700

    feat: clang (#18)
    CLANG SUPREMANCY !! !

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [ git vim fastfetch dust bat tree typos eza gnumake wget ];
+  environment.systemPackages = with pkgs; [ git vim fastfetch dust bat tree typos eza gnumake wget clang ];
 
   services.tailscale = {
     enable = true;
