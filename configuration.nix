185eae08f47db549155d31c2227000b24c3dd892
Tristan F. <tristanf@reed.edu>
2024-09-27 21:40:24 -0700

    feat: gcc
    GCC supremnayc!

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [ git vim fastfetch dust bat tree typos eza gnumake wget clang ];
+  environment.systemPackages = with pkgs; [ git vim fastfetch dust bat tree typos eza gnumake wget clang gcc ];
 
   services.tailscale = {
     enable = true;
