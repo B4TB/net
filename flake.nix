1d23202451530efc4a038a956580e88507b66fc2
Tali Auster <me@tali.network>
2024-09-29 08:18:58 -0700

    feat: enable tcp-games.nim via flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,20 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-  outputs = { self, nixpkgs }: {
+  inputs.tcp-games.url = "github:B4TB/tcp-games";
+
+  outputs = { self, nixpkgs, tcp-games }: {
     # named mulaney because numerous crimes were involved but no one seems to
     # care.
     nixosConfigurations.mulaney = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ./hardware-configuration.nix ./fermi ];
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+
+        tcp-games.nixosModules.nim
+        ./fermi
+      ];
     };
   };
 }
