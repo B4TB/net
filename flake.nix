201a6bd9697790444fbda518bf7288bba989d158
tali auster <me@tali.network>
2024-09-30 13:09:15 -0700

    ci: init (#36)
    * feat: add VM package output
    
    * ci: init VM building
    
    Check that the config builds. Don't check for, e.g., network
    connectivity.
    
    * ci: break CI
    
    tests that CI does what I think it does
    
    * Revert "ci: break CI"
    
    This reverts commit 4e6fd48810729fd696e21593edd92cb710e6f57d.

--- flake.nix
+++ flake.nix
@@ -1,21 +1,26 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.tcp-games.url = "github:B4TB/tcp-games";
 
-  outputs = { self, nixpkgs, tcp-games }: {
-    # named mulaney because numerous crimes were involved but no one seems to
-    # care.
-    nixosConfigurations.mulaney = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
+  outputs = { self, nixpkgs, flake-utils, tcp-games }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages.mulaney = self.nixosConfigurations.mulaney.config.system.build.vm;
+      }) // {
+        # named mulaney because numerous crimes were involved but no one seems to
+        # care.
+        nixosConfigurations.mulaney = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./hardware-configuration.nix
 
-        tcp-games.nixosModules.nim
-        tcp-games.nixosModules.fermi
-        tcp-games.nixosModules.images
-      ];
-    };
-  };
+            tcp-games.nixosModules.nim
+            tcp-games.nixosModules.fermi
+            tcp-games.nixosModules.images
+          ];
+        };
+      };
 }
